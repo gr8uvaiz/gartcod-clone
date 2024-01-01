86c25ee8d2e76ced552a5c6a116a4fbae0d68c53
gr8uvaiz <mohammeduvaiz0786@gmail.com>
2024-01-01 14:27:21 +0530

    Fix Images Issue

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const colors = ['#d4d4d4', '#fde047', '#fca5a5'];
 let colorIndex = 0;
 const img = document.getElementById('img-change');
-
+const hostname = location.hostname != '127.0.0.1'; // it means hostname is in production
 function changeColor(){
     document.documentElement.style.setProperty('--change-color',colors[colorIndex] )
-    if(img.src == 'http://127.0.0.1:5500/assets/two.svg'){
-        img.src = 'http://127.0.0.1:5500/assets/chrome.png'
+    console.log(location.hostname)
+    console.log(img.src)
+    if(hostname){
+        if(img.src == `https://gartcod-clone.vercel.app/assets/two.svg`){
+            img.src = 'https://gartcod-clone.vercel.app/assets/chrome.png'
+        }
+        else{
+            img.src = 'https://gartcod-clone.vercel.app/assets/two.svg'   
+        }
     }
     else{
-        img.src = 'http://127.0.0.1:5500/assets/two.svg'
+        if(img.src == `http://127.0.0.1:5500/assets/two.svg`){
+            img.src = 'http://127.0.0.1:5500/assets/chrome.png'
+        }
+        else{
+            img.src = 'http://127.0.0.1:5500/assets/two.svg'   
+        }
     }
+        
+
     colorIndex = (colorIndex + 1) % colors.length;
 }
 setInterval(() => {
